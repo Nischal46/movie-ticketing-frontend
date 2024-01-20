d0765d84c40d1ac7d45fcf631f9b5ee4d334a672
Nischal <baniyanisal12@gmail.com>
2024-01-20 21:19:33 +0545

    Adding comp[ex time management for the booking according to date

--- src/Component/Pages/Details.jsx
+++ src/Component/Pages/Details.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useGetSingleMoviesQuery } from "../../utils/api";
 import { useParams } from "react-router-dom";
 import UserContext from "../../context/UserContext";
@@ -106,19 +106,47 @@ function Button({cube}){
 
 function Timing(){
   const { setUserData } = useContext(UserContext);
-  const [isOpen, setIsOpen] = useState(false)
+  const [isOpen, setIsOpen] = useState(false);
+  const [timingOpen, setTimingOpen] = useState(false);
+  const [timing, setTiming] = useState([]);
 
-  let timing = [
-    '6 : 00 to 8 : 00 AM',
-    '10 : 00 to 12 : 00 PM',
+const getHour = new Date().getHours();
+const getDate = new Date().getDate();
+
+function CheckTodayDate(passdate){
+  if(getDate === passdate && getHour > 6 && getHour < 10){
+    return setTiming([ '10 : 00 to 12 : 00 PM',
     '2 : 00 to 4 : 00 PM',
-    '6 : 00 to 8 : 00 PM'
-  ];
+    '6 : 00 to 8 : 00 PM'])
+  }
+  
+  else if(getDate === passdate && getHour > 10 && getHour < 16){
+    return setTiming([ '2 : 00 to 4 : 00 PM',
+    '6 : 00 to 8 : 00 PM'])
+  }
+  
+  else if(getDate === passdate && getHour > 16 && getHour < 20){
+    console.log('trur trur');
+    return setTiming([  '6 : 00 to 8 : 00 PM'])
+  }
 
+  else if(getDate === passdate && getHour > 20){
+    return setTiming([]);
+  }
+  
+  else {
+    return setTiming([
+      '6 : 00 to 8 : 00 AM',
+      '10 : 00 to 12 : 00 PM',
+      '2 : 00 to 4 : 00 PM',
+      '6 : 00 to 8 : 00 PM'
+    ]);
+  } 
+}
   let months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
   let TicketDate = [];
 
-  for(let i = 0; i < 3; i++){
+  for(let i = 0; i < 4; i++){
     TicketDate.push(`${months[new Date().getMonth()]} ${new Date().getDate() + i} ${new Date().getFullYear()}`)
   }
   console.log(TicketDate);
@@ -131,17 +159,27 @@ function Timing(){
     setIsOpen(false)
   }
 
+  const openTimingModal = (cl) => {
+    setTimingOpen(true);
+    let today;
+    today = cl.split(' ')[1];
+    CheckTodayDate(+today)
+  }
+
   return  (
     <div>
        <h4 style={{margin: '1rem 0'}}>Timing: </h4>
-      <div style={{display: 'flex', flexWrap: 'wrap', justifyContent: 'space-between', flexDirection: 'column'}}>
-        {timing.map((cl, i) => <li key={i} className="timing" onClick={openModal}>{cl}</li>)}
+
+       <div style={{display: 'flex', justifyContent: 'space-between'}}>
+        {TicketDate.map(cl => <li className="date" onClick={() => openTimingModal(cl)}>{cl}</li>)}
       </div>
+     { timingOpen === true && <div style={{display: 'flex', flexWrap: 'wrap', justifyContent: 'space-between', flexDirection: 'column'}}>
+        {timing.length > 0 ? timing.map((cl, i) => <li key={i} className="timing" onClick={openModal}>{cl}</li>) : <p>No more shows for today. All shows are booked.</p>}
+      </div>
+}
         <ModalOpen isOpen={isOpen} onClose={closeModal} />
 
-      <div style={{display: 'flex', justifyContent: 'space-between'}}>
-        {TicketDate.map(cl => <li className="date">{cl}</li>)}
-      </div>
+   
     </div>
   )
 }
@@ -162,14 +200,14 @@ function ModalOpen({isOpen, onClose}){
       <span onClick={onClose}>&times;</span>
 
       <div style={{display: 'flex', margin: '2rem', flexWrap: 'wrap', justifyContent: 'space-between', alignItems: 'center', position: 'relative', padding: '2rem 0'}}>
-      {seat.map((cl, i) => <div key={i} style={{color: 'green', padding: '0.5rem 0.5rem'}} ><i className="fa fa-stop" aria-hidden="true" style={{fontSize: '46px'}}></i></div>)}
+      {seat.map((cl, i) => <div key={i} style={{color: 'green', padding: '0.5rem 1rem'}} ><i className="fa fa-stop" aria-hidden="true" style={{fontSize: '36px'}}></i></div>)}
 
 
       <div className="screen"></div>
       </div>
 
       <div style={{display: 'flex', alignItems: 'center', justifyContent: 'space-around'}}>
-     <div style={{color: 'red'}}><i className="fa fa-stop" aria-hidden="true" style={{fontSize: '36px'}}></i><p style={{color: 'white'}}>Booked</p></div>
+      <div style={{color: 'red'}}><i className="fa fa-stop" aria-hidden="true" style={{fontSize: '36px'}}></i><p style={{color: 'white'}}>Booked</p></div>
       <div style={{color: 'green'}}><i className="fa fa-stop" aria-hidden="true" style={{fontSize: '36px'}}></i><p style={{color: 'white'}}>Available</p></div>
       </div>
     </div>
